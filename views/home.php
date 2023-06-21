90213a936e97917242b314a7b685d89284ea7545
Adam Cała <adam.cala31@gmail.com>
2023-06-21 13:10:01 +0200

    scss setup and basic styles

--- views/home.php
+++ views/home.php
@@ -1,1 +1,20 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product List</title>
+    <link rel="stylesheet" href="public\css\home.css">
+</head>
+<body>
+    <nav>
+        <h1>Product List</h1>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+        Scandiweb Test assignment
+    </footer>
+</body>
+</html>
--- views/product.php
+++ views/product.php
@@ -1,1 +1,20 @@
-<h1>Product</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+    <link rel="stylesheet" href="public\css\product.css">
+</head>
+<body>
+    <nav>
+        <h1>Product Add</h1>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+        Scandiweb Test assignment
+    </footer>
+</body>
+</html>
