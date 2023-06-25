3c57dbb6548c17ea68dcb29c96225ef7f1e6d49a
Adam Cała <adam.cala31@gmail.com>
2023-06-25 14:21:38 +0200

    product entry css adjustments

--- classes/ProductDisplay.classes.php
+++ classes/ProductDisplay.classes.php
@@ -14,17 +14,19 @@
                 <?php foreach ($product as $product_entry): ?>
                     <div id="<?= $product_entry['SKU']; ?>" class="product-entry-div">
                         <input type="checkbox" name="<?= $product_entry['SKU']; ?>" class="delete-checkbox"<?= $product_entry['SKU']; ?> form="delete_form">
-                        <p><?= $product_entry['SKU']; ?></p>
-                        <p><?= $product_entry['Name']; ?></p>
-                        <p><?= $product_entry['Price']; ?> $</p>
-                        <?php if (count($product_entry['Attributes']) > 1): ?>
-                            Dimension:
-                            <?= implode('x', $product_entry['Attributes']); ?>
-                        <?php else: ?>
-                            <?php foreach ($product_entry['Attributes'] as $Attribute => $value): ?>
-                                <?= $Attribute; ?>:  <?= $value; ?>
-                            <?php endforeach; ?>
-                        <?php endif; ?>
+                        <div>
+                            <p><?= $product_entry['SKU']; ?></p>
+                            <p><?= $product_entry['Name']; ?></p>
+                            <p><?= $product_entry['Price']; ?> $</p>
+                            <?php if (count($product_entry['Attributes']) > 1): ?>
+                                Dimension:
+                                <?= implode('x', $product_entry['Attributes']); ?>
+                            <?php else: ?>
+                                <?php foreach ($product_entry['Attributes'] as $Attribute => $value): ?>
+                                    <?= $Attribute; ?>:  <?= $value; ?>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 <?php endforeach; ?>
             </form>
