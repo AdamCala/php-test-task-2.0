8a4156911818d18e4db6ccf2415100b3affd7049
Adam Cała <adam.cala31@gmail.com>
2023-06-25 18:31:43 +0200

    Add/Cancel buttons work

---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+const add_button_link = document.getElementById('add-button-link');
+
+add_button_link.addEventListener('click',()=>{
+    window.location.href = '/test-task-php2.0/add-product';
+});
